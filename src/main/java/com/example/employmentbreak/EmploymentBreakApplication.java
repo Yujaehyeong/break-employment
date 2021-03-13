fa686f7be12ab681ca1fdfb28b8085d1fe8e2cc4
Yujaehyeong <mynameisyjh@gmail.com>
2021-03-13 15:39:21 +0900

    환경 구축

---
+++ src/main/java/com/example/employmentbreak/EmploymentBreakApplication.java
@@ -0,0 +1,13 @@
+package com.example.employmentbreak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentBreakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmploymentBreakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employmentbreak/EmploymentBreakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employmentbreak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentBreakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
